9d53d19db14b6074f70350fe341dc0568d7eee05
je <je@jm.com>
2021-01-28 10:56:14 +0800

    init

---
+++ AutoOrderMeal.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import time
+import pyautogui
+import sys
+# import keyboard
+
+
+def IsFind(img):
+    location = pyautogui.locateOnScreen(img, confidence=0.8)
+    if location == None:
+        return False
+    else:
+        c = pyautogui.center(location)
+        pyautogui.moveTo(c.x, c.y)
+        return True
+
+
+def SelectNextCourse():
+    ret = pyautogui.locateOnScreen('next.png', confidence=0.8)
+
+    # if ret == None:
+    #     ret = pyautogui.locateOnScreen('icon.png', confidence=0.8)
+
+    if ret == None:
+        return False
+    else:
+        print("select next")
+        p = pyautogui.center(ret)
+        pyautogui.moveTo(p.x, p.y)
+        time.sleep(1)
+        pyautogui.click()
+        return True
+
+
+while True:
+    if(IsFind('wework.png')):
+        print("1")
+        pyautogui.doubleClick()
+        break
+    time.sleep(1)
+
+
+while True:
+    if(IsFind('apps.png')):
+        print("2")
+        pyautogui.click()
+        break
+    time.sleep(1)
+
+
+while True:
+    if(IsFind('order.png')):
+        print("3")
+        pyautogui.click()
+        break
+    time.sleep(1)
+
+while True:
+    if(IsFind('confirm.png')):
+        print("4")
+        pyautogui.click()
+        break
+    time.sleep(1)
