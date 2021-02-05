1a4119711c11f37c0a7cfc92f06a5a88d80d400c
je <je@jm.com>
2021-02-05 10:39:05 +0800

    解决app图标按钮已被选中的情况

--- AutoOrderMeal.py
+++ AutoOrderMeal.py
@@ -7,6 +7,7 @@ import sys
 
 
 def FingImg(img):
+    print("finding {}".format(img))
     location = pyautogui.locateOnScreen(img, confidence=0.8)
     if location == None:
         return False
@@ -16,14 +17,34 @@ def FingImg(img):
         return True
 
 
+def WaitUntilFindImg_Click(img):
+    while True:
+        if(FingImg(img)):
+            print(img, ' found')
+            pyautogui.click()
+            break
+    time.sleep(1)
+
+
+def WaitUntilFindImg_DoubleClick(img):
+    while True:
+        if(FingImg(img)):
+            print(img, ' found')
+            pyautogui.doubleClick()
+            break
+    time.sleep(1)
+
+
+WaitUntilFindImg_DoubleClick('wework.png')
+
 while True:
-    if(FingImg('wework.png')):
-        print("1")
-        pyautogui.doubleClick()
+    if(FingImg('app1.png') or FingImg('app2.png')):
+        print('app', ' found')
+        pyautogui.click()
         break
     time.sleep(1)
 
-imgs = ['apps.png', 'wifi.png', 'order.png', 'confirm.png']
+imgs = ['wifi.png', 'order.png', 'confirm.png']
 
 while True:
     for img in imgs:
